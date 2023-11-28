884e7a581d3e03310cd568bae38d8614f99b9dc2
Apostolos Tzinas <tzinas@tzinas.com>
2023-11-28 19:28:41 +0200

    Add biased gamma concentration and fix cdfs

--- stochastic/chernoff.py
+++ stochastic/chernoff.py
@@ -9,7 +9,7 @@ DURATION = 1000
 
 def create_gamma_distr(α, β):
   def gamma_cdf(x):
-    return β**α * gammainc(α, β * x)
+    return gammainc(α, β * x)
 
   return gamma_cdf, α / β
 
@@ -19,6 +19,16 @@ def create_binomial_distr(n, p):
 
   return binomial_cdf, n * p
 
+def create_biased_gamma_distr(α, β, γ):
+  def gamma_cdf(x):
+    return gammainc(α, β * x)
+
+  def biased_gamma_cdf(x):
+    print(gamma_cdf(max(x - α * γ, 0)))
+    return gamma_cdf(max(x - α * γ, 0))
+
+  return biased_gamma_cdf, γ * α + α / β
+
 def get_tail_probability(cdf, mean):
   tailpoint = mean * (1 + error)
   return 1 - cdf(tailpoint)
@@ -26,15 +36,21 @@ def get_tail_probability(cdf, mean):
 n = np.linspace(1, DURATION, DURATION)
 p_binomial = np.zeros(DURATION)
 p_gamma = np.zeros(DURATION)
+p_biased_gamma = np.zeros(DURATION)
+
+γ = 10
+λ = 1.1
 
 for i in range(1, DURATION + 1):
   binomial_cdf, binomial_mean = create_binomial_distr(i, 0.5)
-  gamma_cdf, gamma_mean = create_gamma_distr(i, np.emath.log(2))
+  gamma_cdf, gamma_mean = create_gamma_distr(i, λ)
+  biased_gamma_cdf, biased_gamma_mean = create_biased_gamma_distr(i, λ, γ)
   p_binomial[i - 1] = get_tail_probability(binomial_cdf, binomial_mean)
   p_gamma[i - 1] = get_tail_probability(gamma_cdf, gamma_mean)
-  print(p_gamma[i - 1])
+  p_biased_gamma[i - 1] = get_tail_probability(biased_gamma_cdf, biased_gamma_mean)
 plt.plot(n, p_binomial, label='Binomial')
 plt.plot(n, p_gamma, label='Gamma')
+plt.plot(n, p_biased_gamma, label=r'Biased Gamma $\gamma = {}$'.format(γ))
 plt.title(r'Prob of value exceeding $10\%$ of mean')
 y = np.exp(-n*(0.01/3))
 plt.plot(n, y, label=r'$e^{-\Omega(n)}$')
