b813a99f9a5f9f6faf84f947c1e27d4e75183b43
Apostolos Tzinas <tzinas@tzinas.com>
2024-11-11 18:51:20 +0200

    include create-plots.sh script and plot data

---
+++ plot_scripts/create-plots.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 bitcoin_vs_poem.py --g --file-name bitcoin_vs_poem_beta_0.01_0.45_33_g_0.05_85.0_50_gamma_0_20.0_40_monte_carlo_100000_error_0.1.json
+
+python3 bitcoin_vs_poem.py --bitcoin-vs-poem --file-name bitcoin_vs_poem_beta_0.01_0.45_33_g_0.05_85.0_50_gamma_0_20.0_40_monte_carlo_100000_error_0.1.json
